ace39cfa8f9db4a41455da508d7bd1d6960bf107
郭宇恒 <yuheng-guo@capcom.com>
2021-05-11 16:49:04 +0900

    sample完成

--- src/app/shared/ng-zorro.module.ts
+++ src/app/shared/ng-zorro.module.ts
@@ -10,6 +10,10 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDividerModule} from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   exports: [
@@ -19,7 +23,11 @@ import { NzAvatarModule } from 'ng-zorro-antd/avatar';
     NzMenuModule,
     NzIconModule,
     NzBadgeModule,
-    NzAvatarModule
+    NzAvatarModule,
+    NzDescriptionsModule,
+    NzCardModule,
+    NzDividerModule,
+    NzGridModule
   ]
 })
 export class NgZorroModule { }
